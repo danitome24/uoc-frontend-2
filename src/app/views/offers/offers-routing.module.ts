d65281c99ac18fd42b45455792557766e1618094
Daniel Tome Fernandez <danieltomefer@gmail.com>
2020-03-11 20:11:48 +0100

    Offer routing working

--- src/app/views/offers/offers-routing.module.ts
+++ src/app/views/offers/offers-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { OffersComponent } from './offers.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: OffersComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
