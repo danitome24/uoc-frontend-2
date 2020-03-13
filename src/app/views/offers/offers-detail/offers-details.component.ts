4b946cd976e296ae8f0671b7183762d4e9d67086
Daniel Tome Fernandez <danieltomefer@gmail.com>
2020-03-13 15:39:17 +0100

    Added missing files

---
+++ src/app/views/offers/offers-detail/offers-details.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Offer } from '../../../shared/models/offer.model';
+
+@Component({
+  selector: 'app-offers-details',
+  templateUrl: './offers-details.component.html',
+  styleUrls: ['./offers-details.component.scss']
+})
+export class OffersDetailsComponent implements OnInit {
+  public offer: Offer;
+
+  constructor(private activatedRoute: ActivatedRoute) {
+  }
+
+  ngOnInit(): void {
+    this.activatedRoute.data.subscribe(((offer: { offer: Offer }) => {
+      console.log(offer.offer);
+      this.offer = offer.offer;
+    }));
+  }
+
+}
