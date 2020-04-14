5c1fd40598f1e5d435c5c995ac6257cc9ec6d47a
Daniel Tome Fernandez <danieltomefer@gmail.com>
2020-04-14 22:43:47 +0200

    Show my offers from store

--- src/app/offers/offers-profile/offers-profile.component.ts
+++ src/app/offers/offers-profile/offers-profile.component.ts
@@ -1,21 +1,28 @@
-import { Component, OnInit } from '@angular/core';
-import { OffersService } from 'src/app/shared/services/offers.service';
-import { Offer } from 'src/app/shared/models/offer.model';
-import { ProfileService } from 'src/app/shared/services/profile.service';
+import {Component, OnInit} from '@angular/core';
+import {Offer} from 'src/app/shared/models/offer.model';
+import {select, Store} from '@ngrx/store';
+import * as fromMyOffers from '../reducers/my-offers.reducer';
+import * as fromOffer from '../actions/offer.actions';
 
 @Component({
-  selector: 'app-offers-profile',
-  templateUrl: './offers-profile.component.html'
+    selector: 'app-offers-profile',
+    templateUrl: './offers-profile.component.html'
 })
 export class OffersProfileComponent implements OnInit {
-  offers: Offer[] = [];
-  constructor(private profileService: ProfileService) {
-    this.selectOffers();
-  }
+    offers: Offer[] = [];
+    private offers$;
 
-  private selectOffers() {
-    this.offers = this.profileService.user.offers;
-  }
+    constructor(private store: Store) {
+        this.selectOffers();
+    }
 
-  ngOnInit() {}
+    private selectOffers() {
+        this.store.dispatch(fromOffer.actions.listOffers({}));
+        this.offers$ = this.store.pipe(
+            select(fromMyOffers.selectMyOffers),
+        );
+    }
+
+    ngOnInit() {
+    }
 }
--- src/app/offers/reducers/my-offers.reducer.ts
+++ src/app/offers/reducers/my-offers.reducer.ts
@@ -1,4 +1,7 @@
 import * as fromMyOffers from '../actions/my-offers.actions';
+import {State as OfferState} from '../reducers/offer.reducer';
+import {createFeatureSelector, createSelector} from '@ngrx/store';
+import {Offer} from '../../shared/models/offer.model';
 
 export interface State {
     entities: [] | number[];
@@ -18,3 +21,16 @@ export function reducer(state: State = initialState, action) {
             return state;
     }
 }
+
+// Selectors
+export const selectMyOffersFeature = createFeatureSelector('my_offers');
+export const selectOffersFeature = createFeatureSelector('offers');
+export const selectMyOffers = createSelector(
+    selectMyOffersFeature,
+    selectOffersFeature,
+    (myOffers: State, offers: OfferState) => {
+        return offers.entities.filter((offer: Offer) => {
+            return myOffers.entities.some(myOfferId => myOfferId === offer.id);
+        });
+    }
+);
