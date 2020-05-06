2065c91659b4b66d2978388df8a8986d332c7a5c
Daniel Tome Fernandez <danieltomefer@gmail.com>
2020-05-06 18:16:50 +0200

    Using MaterialModule

--- src/app/auth/signin/signin.module.ts
+++ src/app/auth/signin/signin.module.ts
@@ -4,20 +4,14 @@ import {SigninRoutingModule} from './signin-routing.module';
 import {SigninComponent} from './signin.component';
 import {SharedModule} from 'src/app/shared/shared.module';
 import {SigninService} from './signin.service';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatInputModule} from '@angular/material/input';
-import {MatCardModule} from '@angular/material/card';
-import {MatButtonModule} from '@angular/material/button';
+import {MaterialModule} from '../../shared/material.module';
 
 @NgModule({
     declarations: [SigninComponent],
     imports: [
         SharedModule,
         SigninRoutingModule,
-        MatFormFieldModule,
-        MatInputModule,
-        MatCardModule,
-        MatButtonModule
+        MaterialModule
     ],
     providers: [SigninService]
 })
